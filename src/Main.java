1e47acd43b01c3a09a3f0f32d622157879a2432e
Tuemin <ngaanh2.2510@gmail.com>
2023-08-28 23:59:48 +0700

    Tach nhanh

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int numberInt = 1;
-        float numberFloat = 1.3f;
-        double numberDouble = 1.2333333333333434343435545;
-        boolean isResult = true;  // or false;
-        char text = 'A';
-        // in kiểu 1
-        System.out.println(numberInt);
-        System.out.println(numberFloat);
-        System.out.println(numberDouble);
-        System.out.println(isResult);
-        System.out.println(text);
 
-        // in kiểu 2
-        System.out.print(numberInt);
-        System.out.print(numberFloat);
-        System.out.print(numberDouble);
-        System.out.print(isResult);
-        System.out.print(text);
-
+        System.out.println("bai tap 1");
 
     }
 }
