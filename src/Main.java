736b950d84918d7cfccb2ac2129841d7841341b5
Tuemin <ngaanh2.2510@gmail.com>
2023-08-28 23:42:26 +0700

    update Nga-lÃesson 1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int numberInt = 1;
+        float numberFloat = 1.3f;
+        double numberDouble = 1.2333333333333434343435545;
+        boolean isResult = true;  // or false;
+        char text = 'A';
+        // in kiểu 1
+        System.out.println(numberInt);
+        System.out.println(numberFloat);
+        System.out.println(numberDouble);
+        System.out.println(isResult);
+        System.out.println(text);
+
+        // in kiểu 2
+        System.out.print(numberInt);
+        System.out.print(numberFloat);
+        System.out.print(numberDouble);
+        System.out.print(isResult);
+        System.out.print(text);
+
+
+    }
+}
